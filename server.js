e650e296ac67c85ad68f55b79e156c416e2c50a6
Ken Hoff <ken@hoff.tech>
2015-12-20 00:46:15 -0700

    first whack at rethinkdb connectinos and stuff

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var jwt = require('express-jwt');
 var cors = require('cors');
+var r = require('rethinkdb');
+var fs = require('fs');
 port = process.env.PORT || 1310
 
 var jwtCheck = jwt({
@@ -8,18 +10,60 @@ var jwtCheck = jwt({
 	audience: 'JeIT5hdK0PXWuMVE1GSYbDT4Uw2HQpKx'
 });
 
+
 // cors lets us call api.barflyorders.com from barflyorders.com
 app.use(cors())
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
 	res.send(200)
 })
 
-app.get("/orders", jwtCheck, function (req, res) {
-	console.log("got request for orders");
-	res.json({orders: "very yes"})
+app.get("/orders", jwtCheck, function(req, res) {
+	onConnect(function (connection) {
+		r.table('orders').filter(r.row('user').eq(req.user.user_id)).run(connection, function (err, cursor) {
+			if (err) {
+				throw err;
+			}
+			else {
+				cursor.toArray(function (err, result) {
+					if (err) throw err
+					res.json(result)
+				})
+			}
+		})
+	})
 })
 
-app.listen(port, function () {
+app.post("/orders", jwtCheck, function (req, res) {
+	userID = req.user.user_id
+	onConnect(function (connection) {
+		r.table('orders').insert({asdf:"1234", user: userID}).run(connection, function (err, result) {
+			res.send(200)
+		})
+	})
+})
+
+app.listen(port, function() {
 	console.log("Listening on", port);
 })
+
+onConnect = function (cb) {
+	fs.readFile("./cacert", function(err, caCert) {
+		r.connect({
+			host: "aws-us-east-1-portal.9.dblayer.com",
+			port: 10384,
+			authKey: "auwvteoiuclrdkxjEofivucXKYESOalvkjcetdlfxkm",
+			db: "barfly_production",
+			ssl: {
+				ca: caCert
+			}
+		}, function(err, conn) {
+			if (!err) {
+				cb(conn)
+			}
+			else {
+				throw "Database connection error"
+			}
+		})
+	})
+}
