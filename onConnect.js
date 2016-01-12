5c3d53352e3db394fbb4300cf4cf19346b174b0a
Ken Hoff <ken@hoff.tech>
2016-01-12 11:16:46 -0700

    moved cacert to dotenv

--- onConnect.js
+++ onConnect.js
@@ -1,23 +1,19 @@
-var fs = require('fs');
 var r = require('rethinkdb');
 
 module.exports = function(cb) {
-	// console.log("opening new connection");
-	fs.readFile("./cacert", function(err, caCert) {
-		r.connect({
-			host: "aws-us-east-1-portal.9.dblayer.com",
-			port: 10384,
-			authKey: "auwvteoiuclrdkxjEofivucXKYESOalvkjcetdlfxkm",
-			db: "barfly_production",
-			ssl: {
-				ca: caCert
-			}
-		}, function(err, conn) {
-			if (!err) {
-				cb(conn)
-			} else {
-				throw "Database connection error"
-			}
-		})
+	r.connect({
+		host: "aws-us-east-1-portal.9.dblayer.com",
+		port: 10384,
+		authKey: "auwvteoiuclrdkxjEofivucXKYESOalvkjcetdlfxkm",
+		db: "barfly_production",
+		ssl: {
+			ca: new Buffer(process.env.RETHINKDB_CACERT)
+		}
+	}, function(err, conn) {
+		if (!err) {
+			cb(conn)
+		} else {
+			throw "Database connection error"
+		}
 	})
 }
