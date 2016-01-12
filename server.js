9f55cb5f26557fed6b3f6db4359337001dada334
Ken Hoff <ken@hoff.tech>
2016-01-12 10:41:22 -0700

    starting to integrate dotenv

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').load()
 var app = require('express')();
 var jwt = require('express-jwt');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var request = require('request');
-port = process.env.PORT || 1310
+port = process.env.PORT
 
 // cors lets us call api.barflyorders.com from barflyorders.com
 app.use(cors())
