7cec60597407d057ad4ba59718ff8417811ba81a
Ken Hoff <ken@hoff.tech>
2016-01-15 15:42:27 -0700

    renaming evs

--- jwtCheck.js
+++ jwtCheck.js
@@ -1,6 +1,6 @@
 var jwt = require('express-jwt');
 
 module.exports = jwt({
-	secret: new Buffer(process.env.AUTH0_CLIENTSECRET, 'base64'),
-	audience: process.env.CLIENTID
+	secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+	audience: process.env.AUTH0_CLIENT_ID
 });
