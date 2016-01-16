456c2931df87603ed1d780d36645786997867923
Ken Hoff <ken@hoff.tech>
2016-01-15 17:09:46 -0700

    adding rethinkdb env vars

--- onConnect.js
+++ onConnect.js
@@ -2,10 +2,10 @@ var r = require('rethinkdb');
 
 if (process.env.NODE_ENV == "production") {
 	opts = {
-		host: "aws-us-east-1-portal.9.dblayer.com",
-		port: 10384,
-		authKey: "auwvteoiuclrdkxjEofivucXKYESOalvkjcetdlfxkm",
-		db: "barfly_production",
+		host: process.env.RETHINKDB_HOST,
+		port: process.env.RETHINKDB_PORT,
+		authKey: process.env.RETHINKDB_AUTH_KEY,
+		db: process.env.RETHINKDB_DB,
 		ssl: {
 			ca: new Buffer(process.env.RETHINKDB_CACERT)
 		}
