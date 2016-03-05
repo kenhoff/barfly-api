3bd7d8580876a7d76be24011fd91d07f577b797f
Ken Hoff <ken@hoff.tech>
2016-03-05 12:50:57 -0700

    looking up user cards in stripe

--- routes/paymentmethods.js
+++ routes/paymentmethods.js
@@ -4,6 +4,11 @@
 var jwtCheck = require("../jwtCheck.js")
 var request = require("request")
 
+
+var stripe = require("stripe")(
+	"sk_test_wjIDkyybByjHF1YTuxCMy7l8"
+)
+
 module.exports = function(app) {
 	app.get("/paymentmethods", jwtCheck, function(req, res) {
 		// have to deliberately look up app_metadata on Auth0 for user
@@ -16,15 +21,24 @@ module.exports = function(app) {
 		}, function(err, response, body) {
 			if (err) {
 				res.status(500).send(err)
-			} else if (response.statusCode < 300) {
+			} else {
 				var user = JSON.parse(body)
 				if (!("app_metadata" in user)) {
 					res.json({})
 				} else if (!("stripe_id" in user.app_metadata)) {
 					res.json({})
+				} else {
+					stripe.customers.listCards(user.app_metadata.stripe_id, function(err, cards) {
+						// asynchronously called
+						if (err) {
+							res.status(500).send(err)
+						} else if (cards.data.length == 0) {
+							res.json({})
+						} else {
+							res.json(cards.data[0])
+						}
+					})
 				}
-			} else {
-				res.status(500).send(err)
 			}
 		})
 	})
