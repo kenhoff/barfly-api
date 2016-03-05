abba9a9809bb4b5e2421bab61db9068aaf485b5d
Ken Hoff <ken@hoff.tech>
2016-03-05 15:50:17 -0700

    more semicolons

--- routes/paymentmethods.js
+++ routes/paymentmethods.js
@@ -1,14 +1,14 @@
 // var r = require('rethinkdb')
 // var onConnect = require('../onConnect.js')
 // var getNextCounter = require('../getNextCounter.js')
-var jwtCheck = require("../jwtCheck.js")
-var request = require("request")
-var async = require("async")
+var jwtCheck = require("../jwtCheck.js");
+var request = require("request");
+var async = require("async");
 
 
 var stripe = require("stripe")(
 	"sk_test_wjIDkyybByjHF1YTuxCMy7l8"
-)
+);
 
 module.exports = function(app) {
 	app.get("/paymentmethods", jwtCheck, function(req, res) {
@@ -21,28 +21,28 @@ module.exports = function(app) {
 			}
 		}, function(err, response, body) {
 			if (err) {
-				res.status(500).send(err)
+				res.status(500).send(err);
 			} else {
-				var user = JSON.parse(body)
+				var user = JSON.parse(body);
 				if (!("app_metadata" in user)) {
-					res.json({})
+					res.json({});
 				} else if (!("stripe_id" in user.app_metadata)) {
-					res.json({})
+					res.json({});
 				} else {
 					stripe.customers.listCards(user.app_metadata.stripe_id, function(err, cards) {
 						// asynchronously called
 						if (err) {
-							res.status(500).send(err)
+							res.status(500).send(err);
 						} else if (cards.data.length == 0) {
-							res.json({})
+							res.json({});
 						} else {
-							res.json(cards.data[0])
+							res.json(cards.data[0]);
 						}
-					})
+					});
 				}
 			}
-		})
-	})
+		});
+	});
 
 	app.post("/paymentmethods", jwtCheck, function(req, res) {
 		request.get({
@@ -52,9 +52,9 @@ module.exports = function(app) {
 			}
 		}, function(err, response, body) {
 			if (err) {
-				res.status(500).send(err)
+				res.status(500).send(err);
 			} else {
-				var user = JSON.parse(body)
+				var user = JSON.parse(body);
 
 				// if a user doesn't already have a stripe id
 				if (!("app_metadata" in user) || !("stripe_id" in user.app_metadata)) {
@@ -64,7 +64,7 @@ module.exports = function(app) {
 						source: req.body.token.id
 					}, function(err, customer) {
 						if (err) {
-							res.status(500).send(err)
+							res.status(500).send(err);
 						} else {
 							request.patch({
 								url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
@@ -78,61 +78,61 @@ module.exports = function(app) {
 								}
 							}, function(err) {
 								if (err) {
-									res.status(500).send(err)
+									res.status(500).send(err);
 								} else {
-									res.sendStatus(200)
+									res.sendStatus(200);
 								}
-							})
+							});
 						}
-					})
+					});
 				} else {
 					// if a user does already have a stripe id, get user's cards
 					stripe.customers.listCards(user.app_metadata.stripe_id, function(err, cards) {
 						// asynchronously called
 						if (err) {
-							res.status(500).send(err)
+							res.status(500).send(err);
 						} else if (cards.data.length == 0) {
 							// if user has no cards, just add a new one
 							stripe.customers.createSource(user.app_metadata.stripe_id, {
 								source: req.body.token.id
 							}, function(err) {
 								if (err) {
-									res.status(500).send(err)
+									res.status(500).send(err);
 								} else {
-									res.sendStatus(200)
+									res.sendStatus(200);
 								}
-							})
+							});
 						} else {
 							// if user has cards, delete old cards, then add new one
 							async.each(cards.data, function(card, cb) {
 								// delete card, call cb
 								stripe.customers.deleteCard(user.app_metadata.stripe_id, card.id, function(err) {
-									cb(err)
-								})
+									cb(err);
+								});
 							}, function(err) {
 								if (err) {
-									res.status(500).send(err)
+									res.status(500).send(err);
 								} else {
 									// add new card
 									stripe.customers.createSource(user.app_metadata.stripe_id, {
 										source: req.body.token.id
 									}, function(err) {
 										if (err) {
-											res.status(500).send(err)
+											res.status(500).send(err);
 										} else {
-											res.sendStatus(200)
+											res.sendStatus(200);
 										}
-									})
+									});
 								}
-							})
+							});
 						}
-					})
+					});
 
 				}
 
 			}
-		})
-	})
+		});
+	});
 
 	app.delete("/paymentmethods", jwtCheck, function(req, res) {
 		// if user doesn't have app_metadata, or if user doesn't have a stripe_id in app_metadata, don't do anything
@@ -144,42 +144,42 @@ module.exports = function(app) {
 			}
 		}, function(err, response, body) {
 			if (err) {
-				res.status(500).send(err)
+				res.status(500).send(err);
 			} else {
-				var user = JSON.parse(body)
+				var user = JSON.parse(body);
 
 				// if a user doesn't have app_metadata or stripe_id
 				if (!("app_metadata" in user) || !("stripe_id" in user.app_metadata)) {
-					res.sendStatus(200)
+					res.sendStatus(200);
 				} else {
 					// if so, check and see if user's got any cards
 					stripe.customers.listCards(user.app_metadata.stripe_id, function(err, cards) {
 						if (err) {
-							res.status(500).send(err)
+							res.status(500).send(err);
 						} else if (cards.data.length == 0) {
 							// if user has no cards, nothing to do
-							res.sendStatus(200)
+							res.sendStatus(200);
 						} else {
 							// if user has cards, delete them
 							async.each(cards.data, function(card, cb) {
 								// delete card, call cb
 								stripe.customers.deleteCard(user.app_metadata.stripe_id, card.id, function(err) {
-									cb(err)
-								})
+									cb(err);
+								});
 							}, function(err) {
 								if (err) {
-									res.status(500).send(err)
+									res.status(500).send(err);
 								} else {
-									res.sendStatus(200)
+									res.sendStatus(200);
 								}
-							})
+							});
 						}
-					})
+					});
 				}
 			}
-		})
+		});
 
 
 
-	})
-}
+	});
+};
