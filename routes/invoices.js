4d1c8e49c56398c51646675bc8ae70166c036a8b
Ken Hoff <ken@hoff.tech>
2016-03-22 14:45:38 -0600

    handling empty user app_metadata and stripe_id

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,5 +1,5 @@
 var jwtCheck = require('../jwtCheck.js');
-// var request = require('request');
+var request = require('request');
 // var async = require('async');
 
 var stripe = require("stripe")(
@@ -9,18 +9,46 @@ var stripe = require("stripe")(
 
 module.exports = function(app) {
 	app.get("/invoices", jwtCheck, function(req, res) {
-		stripe.invoices.list({
-			customer: req.user.app_metadata.stripe_id,
-			limit: 1
-		}, function(err, invoices) {
+
+		// look up user
+		request.get({
+			url: "https://" + process.env.AUTH0_DOMAIN + "/api/v2/users/" + req.user.sub,
+			headers: {
+				"Authorization": "Bearer " + process.env.AUTH0_API_JWT
+			}
+		}, function(err, response, body) {
 			if (err) {
 				res.status(500).send(err);
 			} else {
-				// just for testing
-				invoices.data[0].paid = false;
-				invoices.data[0].amount_due = 3000;
-				res.status(200).send(invoices.data[0]);
+				var user = JSON.parse(body);
+				if (("app_metadata" in user) && ("stripe_id" in user.app_metadata)) {
+					stripe.invoices.list({
+						customer: user.app_metadata.stripe_id,
+						limit: 1
+					}, function(err, invoices) {
+						if (err) {
+							res.status(500).send(err);
+						} else {
+							if (invoices.data.length != 0) {
+								// just for testing
+								invoices.data[0].paid = false;
+								invoices.data[0].amount_due = 3000;
+								res.status(200).send(invoices.data[0]);
+							} else {
+								res.status(200).send({});
+							}
+						}
+					});
+				} else {
+					res.status(200).send({});
+				}
 			}
 		});
+
+
+
+
+
+
 	});
 };
